b6bedb4430ac5a61412cfebfd08ac7664299b211
unknown <machineherald007@gmail.com>
2024-04-09 05:38:17 -0400

    adds http files

---
+++ src/http/logged_in.js
@@ -0,0 +1,21 @@
+const axios = require("axios")
+
+function postMessage() {
+    return axios({
+        url: process.env.BANNER_BOT_WORKER_ENDPOINT,
+        method: "POST",
+        data: JSON.stringify({
+            messages: [{
+                msg: "LOGGED_IN"
+            }]
+        }),
+        headers: {
+            Authorization: `${process.env.QUEUE_AUTH_SECRET}`,
+            "Content-Type": "application/json"
+        }
+    })
+}
+
+postMessage()
+.then(res => console.log(res.data))
+.catch(err => console.log(err))
